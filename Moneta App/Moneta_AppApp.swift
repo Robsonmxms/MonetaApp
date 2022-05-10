819ee5f18b960d1e76cdabd7f77cd6a7dff0c53b
Robson Lima Lopes <robson060814@hotmail.com>
2022-05-10 08:38:35 -0300

    Initial Commit

---
+++ Moneta App/Moneta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moneta_AppApp.swift
+//  Moneta App
+//
+//  Created by Robson Lopes on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Moneta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
